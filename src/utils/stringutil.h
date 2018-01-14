8e9385cb72208e16544d3b9239bed748d047f31a
Axel Isouard <axel@isouard.fr>
2018-01-14 17:26:49 +0100

    Utils: Fix style check

--- src/utils/stringutil.h
+++ src/utils/stringutil.h
@@ -1,5 +1,27 @@
-#ifndef STRINGUTIL_H
-#define STRINGUTIL_H
+/*
+ * Copyright 2018 Axel Isouard <axel@isouard.fr>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef UTILS_STRINGUTIL_H_
+#define UTILS_STRINGUTIL_H_
 
 #include "public/ustring.h"
 
@@ -8,10 +30,11 @@ using Public::UStringVector;
 
 namespace Utils {
 class StringUtil {
-public:
-  static void Concat(const UStringVector &input, const UString &glue, UString &output);
-  static void Tokenize(const UString &str, UStringVector &output);
+ public:
+  static void Concat(const UStringVector &input, const UString &glue,
+                     UString *output);
+  static void Tokenize(const UString &str, UStringVector *output);
 };
-}
+}  // namespace Utils
 
-#endif //STRINGUTIL_H
+#endif  // UTILS_STRINGUTIL_H_
--- src/utils/stringutil_unittest.cpp
+++ src/utils/stringutil_unittest.cpp
@@ -1,12 +1,34 @@
-#include "gtest/gtest.h"
-#include "stringutil.h"
+/*
+ * Copyright 2018 Axel Isouard <axel@isouard.fr>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 
-using namespace Utils;
+#include "gtest/gtest.h"
+#include "utils/stringutil.h"
+
+using Utils::StringUtil;
 
 TEST(StringUtil_Tokenize, SingleWord) {
   UStringVector output;
 
-  StringUtil::Tokenize("hello", output);
+  StringUtil::Tokenize("hello", &output);
 
   ASSERT_EQ(output.size(), 1);
   ASSERT_EQ(output[0], UString::fromUTF8("hello"));
@@ -15,7 +37,7 @@ TEST(StringUtil_Tokenize, SingleWord) {
 TEST(StringUtil_Tokenize, TwoWords) {
   UStringVector output;
 
-  StringUtil::Tokenize("hello world!", output);
+  StringUtil::Tokenize("hello world!", &output);
 
   ASSERT_EQ(output.size(), 2);
   ASSERT_EQ(output[0], UString::fromUTF8("hello"));
@@ -25,7 +47,7 @@ TEST(StringUtil_Tokenize, TwoWords) {
 TEST(StringUtil_Tokenize, TwoWordsWhiteSpacesMiddle) {
   UStringVector output;
 
-  StringUtil::Tokenize("hello     world!", output);
+  StringUtil::Tokenize("hello     world!", &output);
 
   ASSERT_EQ(output.size(), 2);
   ASSERT_EQ(output[0], UString::fromUTF8("hello"));
@@ -35,7 +57,7 @@ TEST(StringUtil_Tokenize, TwoWordsWhiteSpacesMiddle) {
 TEST(StringUtil_Tokenize, TwoWordsWhiteSpacesEnding) {
   UStringVector output;
 
-  StringUtil::Tokenize("hello world!   ", output);
+  StringUtil::Tokenize("hello world!   ", &output);
 
   ASSERT_EQ(output.size(), 2);
   ASSERT_EQ(output[0], UString::fromUTF8("hello"));
@@ -45,7 +67,7 @@ TEST(StringUtil_Tokenize, TwoWordsWhiteSpacesEnding) {
 TEST(StringUtil_Tokenize, TwoWordsWhiteSpacesBoth) {
   UStringVector output;
 
-  StringUtil::Tokenize("hello     world!   ", output);
+  StringUtil::Tokenize("hello     world!   ", &output);
 
   ASSERT_EQ(output.size(), 2);
   ASSERT_EQ(output[0], UString::fromUTF8("hello"));
@@ -55,7 +77,7 @@ TEST(StringUtil_Tokenize, TwoWordsWhiteSpacesBoth) {
 TEST(StringUtil_Tokenize, TwoWordsHangul) {
   UStringVector output;
 
-  StringUtil::Tokenize("연인이나 친구", output);
+  StringUtil::Tokenize("연인이나 친구", &output);
 
   ASSERT_EQ(output.size(), 2);
   ASSERT_EQ(output[0], UString::fromUTF8("연인이나"));
@@ -65,7 +87,7 @@ TEST(StringUtil_Tokenize, TwoWordsHangul) {
 TEST(StringUtil_Tokenize, ThreeWordsDoubleQuotes) {
   UStringVector output;
 
-  StringUtil::Tokenize("hello world \"nice\"", output);
+  StringUtil::Tokenize("hello world \"nice\"", &output);
 
   ASSERT_EQ(output.size(), 3);
   ASSERT_EQ(output[0], UString::fromUTF8("hello"));
@@ -76,7 +98,7 @@ TEST(StringUtil_Tokenize, ThreeWordsDoubleQuotes) {
 TEST(StringUtil_Tokenize, ThreeWordsSingleDoubleQuotes) {
   UStringVector output;
 
-  StringUtil::Tokenize("hello world \"nice", output);
+  StringUtil::Tokenize("hello world \"nice", &output);
 
   ASSERT_EQ(output.size(), 3);
   ASSERT_EQ(output[0], UString::fromUTF8("hello"));
@@ -87,7 +109,7 @@ TEST(StringUtil_Tokenize, ThreeWordsSingleDoubleQuotes) {
 TEST(StringUtil_Tokenize, ThreeWordsDoubleQuotesWhitespaces) {
   UStringVector output;
 
-  StringUtil::Tokenize("hello world \"nice  to meet   you\"", output);
+  StringUtil::Tokenize("hello world \"nice  to meet   you\"", &output);
 
   ASSERT_EQ(output.size(), 3);
   ASSERT_EQ(output[0], UString::fromUTF8("hello"));
@@ -98,7 +120,8 @@ TEST(StringUtil_Tokenize, ThreeWordsDoubleQuotesWhitespaces) {
 TEST(StringUtil_Tokenize, EscapedDoubleQuotes) {
   UStringVector output;
 
-  StringUtil::Tokenize("\\\"hello world \\\" \"nice to \\\"meet\\\" you\"", output);
+  StringUtil::Tokenize("\\\"hello world \\\" \"nice to \\\"meet\\\" you\"",
+                       &output);
 
   ASSERT_EQ(output.size(), 4);
   ASSERT_EQ(output[0], UString::fromUTF8("\"hello"));
@@ -110,7 +133,7 @@ TEST(StringUtil_Tokenize, EscapedDoubleQuotes) {
 TEST(StringUtil_Tokenize, DoubleQuotesEscapedDoubleQuotes) {
   UStringVector output;
 
-  StringUtil::Tokenize("hello world \"nice to \\\"meet\\\" you\"", output);
+  StringUtil::Tokenize("hello world \"nice to \\\"meet\\\" you\"", &output);
 
   ASSERT_EQ(output.size(), 3);
   ASSERT_EQ(output[0], UString::fromUTF8("hello"));
